cc7aea94875616c998d34a46024ee1cf37920a5d
Simon Goller <simon.goller4@gmail.com>
2021-05-18 17:22:07 +0200

    Tried to change something

--- Habit Hive/Habit Hive/ContentView.swift
+++ Habit Hive/Habit Hive/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 import CoreData
 
+//hallo sebastian
+
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
@@ -42,8 +44,7 @@ struct ContentView: View {
             do {
                 try viewContext.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
